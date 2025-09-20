c1151717e8bcaa826ee3764840c8a424efd771af
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-20 17:05:38 +0800

    refactor: rename field names

--- Playwright.SauceDemo/Constants/Login/Field_Login.cs
+++ Playwright.SauceDemo/Constants/Login/Field_Login.cs
@@ -2,9 +2,9 @@
 {
     internal class Field_Login
     {
-        public const string Login_Username = "login-username";
-        public const string Login_Password = "login-password";
-        public const string Login_Btn = "login-button";
-        public const string Login_ErrMessage = "login-error-message";
+        public const string LOGIN_USERNAME = "login-username";
+        public const string LOGIN_PASSWORD = "login-password";
+        public const string LOGIN_BUTTON = "login-button";
+        public const string LOGIN_ERROR_MESSAGE = "login-error-message";
     }
 }
