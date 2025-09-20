bc56ffdf16a0d5fa31212ed074370144bfe82fb9
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-20 17:49:21 +0800

    refactor: rename ConfigLoader class file.

---
+++ Playwright.SauceDemo/Utils/Util_ConfigLoader.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Playwright.SauceDemo.Utils
+{
+    internal static class Util_ConfigLoader
+    {
+        public static T Get<T>(string fileName = "appsettings.json")
+        {
+            var config = new ConfigurationBuilder();
+        }
+    }
+}
