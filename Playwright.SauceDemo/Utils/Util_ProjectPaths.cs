bcf341d28edd0197779c3e5697bdc90834c9564b
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-20 17:59:05 +0800

    feat: implement get path methods in ProjectPaths util class.

--- Playwright.SauceDemo/Utils/Util_ProjectPaths.cs
+++ Playwright.SauceDemo/Utils/Util_ProjectPaths.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.SauceDemo.Utils
+﻿namespace Playwright.SauceDemo.Utils
 {
-    internal class Util_ProjectPaths
+    internal static class Util_ProjectPaths
     {
+        /// <summary>
+        /// Returns the appsettings path
+        /// </summary>
+        public static string GetConfigPath() => GetProjectRoot();
+
+        private static string GetProjectRoot()
+        {
+            // First check: walk up the directories until csproj is found.
+            var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            while (currentDirectory != null)
+            {
+                if (currentDirectory.GetFiles("*.csproj").Any())
+                    return currentDirectory.FullName;
+
+                currentDirectory = currentDirectory.Parent;
+            }
+
+            // Second check: return current directory.
+            return Directory.GetCurrentDirectory();
+        }
     }
 }
