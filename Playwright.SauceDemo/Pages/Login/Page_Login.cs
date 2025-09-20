871cc00682c54594958ef55a2fa8d8f549d7d1f8
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-20 16:00:18 +0800

    feat: add new folders 'Pages' > 'Login', and added Page_Login class file.

---
+++ Playwright.SauceDemo/Pages/Login/Page_Login.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+namespace Playwright.SauceDemo.Pages.Login
+{
+    internal class Page_Login
+    {
+        private readonly IPage _page;
+
+        public Page_Login(IPage page)
+        {
+            _page = page;
+        }
+    }
+}
