864c9cf4e5d905b87c91a3b5e74c0618714a2e6f
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-20 17:02:34 +0800

    feat: implement constant fields for 'Page_Login' class

--- Playwright.SauceDemo/Constants/Login/Field_Login.cs
+++ Playwright.SauceDemo/Constants/Login/Field_Login.cs
@@ -2,5 +2,9 @@
 {
     internal class Field_Login
     {
+        public const string Login_Username = "login-username";
+        public const string Login_Password = "login-password";
+        public const string Login_Btn = "login-button";
+        public const string Login_ErrMessage = "login-error-message";
     }
 }
