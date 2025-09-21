4398899fbcf9181b69845b9206d815808160fd66
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-21 11:31:11 +0800

    feat: implement Setup/Teardown methods in 'BaseTest' class.

--- Playwright.SauceDemo/Tests/BaseTest.cs
+++ Playwright.SauceDemo/Tests/BaseTest.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright.NUnit;
+using Playwright.SauceDemo.Models;
+using Playwright.SauceDemo.Utils;
 
 namespace Playwright.SauceDemo.Tests
 {
-    internal class BaseTest
-    {
-    }
+   internal abstract class BaseTest : PageTest
+   {
+      protected Config _config;
+
+      //Initialize report.
+      [OneTimeSetUp]
+      public void ReportSetup() => Util_ReportManager.CreateExtentReport("SauceDemo");
+
+      [SetUp]
+      public virtual void Setup()
+      {
+         // Load app config settings.
+         _config = Util_ConfigLoader.Load<Config>("appsettings.json", "ConfigSettings");
+
+         // Initialize report test.
+         Util_ReportManager.CreateExtentTest(TestContext.CurrentContext.Test.FullName);
+      }
+
+      // Close report.
+      [OneTimeTearDown]
+      public void ReportClose() => Util_ReportManager.QuitExtentReport();
+   }
 }
