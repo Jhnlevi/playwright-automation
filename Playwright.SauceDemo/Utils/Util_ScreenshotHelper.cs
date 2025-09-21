8d64bc409a0bf5c858f4108180b9eda491d486e5
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-21 13:21:35 +0800

    feat: implement 'CaptureAsync' method in 'ScreenshotHelper' util class.

--- Playwright.SauceDemo/Utils/Util_ScreenshotHelper.cs
+++ Playwright.SauceDemo/Utils/Util_ScreenshotHelper.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
 
 namespace Playwright.SauceDemo.Utils
 {
-   internal class Util_ScreenshotHelper
+   internal static class Util_ScreenshotHelper
    {
+      public static async Task<string> CaptureAsync(IPage page, string name = "")
+      {
+         var directory = Util_ProjectPaths.GetScreenshotPath();
+         Directory.CreateDirectory(directory);
+
+         var defaultName = String.IsNullOrWhiteSpace(name) ? "Screenshot" : name;
+         var fileName = $"{defaultName}_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+         var filePath = Path.Combine(directory, fileName);
+
+         await page.ScreenshotAsync(new PageScreenshotOptions
+         {
+            Path = filePath,
+            FullPage = true
+         });
+
+         return filePath;
+      }
    }
 }
