1456b4dd7d68bfb832fa2486701fe70457073482
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-22 14:32:52 +0800

    feat: implement 'ReadJson' and 'LoadJson' methods in 'JsonHelper' util class.

--- Playwright.SauceDemo/Utils/Util_JsonHelper.cs
+++ Playwright.SauceDemo/Utils/Util_JsonHelper.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 
 namespace Playwright.SauceDemo.Utils
 {
-   internal class Util_JsonHelper
+   internal static class Util_JsonHelper
    {
+      /// <summary>
+      /// Reads the .json file
+      /// </summary>
+      /// <returns>Deserialized json file.</returns>
+      private static T ReadJson<T>(string path)
+      {
+         var data = File.ReadAllText(path);
+         return JsonSerializer.Deserialize<T>(data)!;
+      }
+
+      public static T LoadJson<T>(string module = "", string file = "")
+      {
+         var directory = string.IsNullOrWhiteSpace(module)
+            ? Util_ProjectPaths.GetTestDataPath()
+            : Path.Combine(Util_ProjectPaths.GetTestDataPath(), module);
+
+         if (!Directory.Exists(directory))
+            throw new DirectoryNotFoundException("Test data module folder not found.");
+
+         if (string.IsNullOrWhiteSpace(file))
+            throw new ArgumentException("File name must be provided to load test data.");
+
+         var filePath = Path.Combine(directory, file);
+
+         if (!File.Exists(filePath))
+            throw new FileNotFoundException($"{file} is not found at {directory}");
+
+         return ReadJson<T>(filePath);
+      }
    }
 }
