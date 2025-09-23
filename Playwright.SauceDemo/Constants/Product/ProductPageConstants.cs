8b6be226077755b6dbe5caea00af37a34e7478ce
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-23 23:33:32 +0800

    feat: implement constant properties for product page

--- Playwright.SauceDemo/Constants/Product/ProductPageConstants.cs
+++ Playwright.SauceDemo/Constants/Product/ProductPageConstants.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.SauceDemo.Constants.Product
+﻿namespace Playwright.SauceDemo.Constants.Product
 {
    internal class ProductPageConstants
    {
+      public const string PRODUCT_SORT_DROPDOWN = "product-sort-dropdown";
+      public const string PRODUCT_ITEM_NAME = "product-item-name";
+      public const string PRODUCT_ITEM_DESCRIPTION = "product-item-description";
+      public const string PRODUCT_ITEM_PRICE = "product-item-price";
+      public const string PRODUCT_ADD_TO_CART_BUTTON = "product-add-to-cart-button";
+      public const string PRODUCT_REMOVE_FROM_CART_BUTTON = "product-remove-from-cart-button";
+      public const string PRODUCT_ITEM_LABEL = "product-item-label";
+      public const string PRODUCT_ITEM_IMG = "product-item-image";
+      public const string PRODUCT_PRICEBAR = "product-pricebar";
+      public const string PRODUCT_ITEM_CARD = "product-item-card";
    }
 }
