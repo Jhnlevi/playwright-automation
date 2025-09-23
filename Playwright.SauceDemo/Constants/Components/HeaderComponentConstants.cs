bdbb5dd4435cbc042523581c5698fc165554cb0c
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-23 21:46:27 +0800

    feat: implement constant properties for header component

--- Playwright.SauceDemo/Constants/Components/HeaderComponentConstants.cs
+++ Playwright.SauceDemo/Constants/Components/HeaderComponentConstants.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.SauceDemo.Constants.Components
+﻿namespace Playwright.SauceDemo.Constants.Components
 {
    internal class HeaderComponentConstants
    {
+      public const string HEADER_MENU_BUTTON = "header-menu-button";
+      public const string HEADER_LOGO = "header-logout-link";
+      public const string HEADER_CART_ICON = "header-cart-icon";
+      public const string HEADER_CART_BADGE = "header-cart-badge";
    }
 }
