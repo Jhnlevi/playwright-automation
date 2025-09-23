13cbc78e91161951d7e807b8a008fa6fe1cb0385
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-23 14:19:02 +0800

    feat: add test data filtering methods and new test method with [Category("UI")]

--- Playwright.SauceDemo/Tests/UI/Login/Test_Login.cs
+++ Playwright.SauceDemo/Tests/UI/Login/Test_Login.cs
@@ -18,18 +18,57 @@ namespace Playwright.SauceDemo.Tests.UI.Login
 
          _login = new Page_Login(Page);
 
-         // Navigate to SauceDemo website
          Util_ReportManager.Log(ReportInfo, "Navigating to SauceDemo Website.");
          Page.GotoAsync(_config.BaseUrl);
       }
 
-      [TestCaseSource(typeof(Provider_Login), nameof(Provider_Login.GetPositiveCases))]
-      public async Task Login_VerifyWithValidCredentials(LoginTestCase testCase)
+      // Methods
+      public static IEnumerable<LoginTestCase> GetStandardUser() => Provider_Login.GetPositiveCases().Where(tc => tc.Id == "TC_Login_0001_1");
+
+      public static IEnumerable<LoginTestCase> GetLockedOutUsers() => Provider_Login.GetNegativeCases().Where(tc => tc.Type == "locked_out_user");
+
+      public static IEnumerable<LoginTestCase> GetInvalidCases() => Provider_Login.GetNegativeCases().Where(tc => tc.Type != "locked_out_user");
+
+      // Tests
+      [Category("UI")]
+      [TestCaseSource(nameof(GetStandardUser))]
+      public async Task Login_VerifyStandardUserAccount_ShouldSucceed(LoginTestCase testCase)
       {
+         var data = testCase.Data;
+
+         if (data == null)
+         {
+            Assert.Fail("Test data is null.");
+            return;
+         }
+
          Util_ReportManager.Log(ReportInfo, "Entering Username.");
-         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, testCase.Data!.Username);
+         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, data.Username);
          Util_ReportManager.Log(ReportInfo, "Entering Password.");
-         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, testCase.Data!.Password);
+         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, data.Password);
+         Util_ReportManager.Log(ReportInfo, "Clicking 'Login' button.");
+         await _login.ClickElementAsync(Field_Login.LOGIN_BUTTON);
+         Util_ReportManager.Log(ReportInfo, "Verifying that the user can login with valid credentials and reach 'Inventory' page.");
+         var inventoryContainer = Page.Locator("#inventory_container.inventory_container");
+         await Expect(Page).ToHaveURLAsync("https://www.saucedemo.com/v1/inventory.html");
+         await Expect(inventoryContainer).ToBeVisibleAsync();
+      }
+
+      [TestCaseSource(typeof(Provider_Login), nameof(Provider_Login.GetPositiveCases))]
+      public async Task Login_VerifyWithValidCredentials_ShouldSucceed(LoginTestCase testCase)
+      {
+         var data = testCase.Data;
+
+         if (data == null)
+         {
+            Assert.Fail("Test data is null.");
+            return;
+         }
+
+         Util_ReportManager.Log(ReportInfo, "Entering Username.");
+         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, data.Username);
+         Util_ReportManager.Log(ReportInfo, "Entering Password.");
+         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, data.Password);
          Util_ReportManager.Log(ReportInfo, "Clicking 'Login' button.");
          await _login.ClickElementAsync(Field_Login.LOGIN_BUTTON);
          Util_ReportManager.Log(ReportInfo, "Verifying that the user can login with valid credentials and reach 'Inventory' page.");
@@ -40,40 +79,42 @@ namespace Playwright.SauceDemo.Tests.UI.Login
          await Expect(inventoryContainer).ToBeVisibleAsync();
       }
 
-      [TestCaseSource(typeof(Provider_Login), nameof(Provider_Login.GetNegativeCases))]
-      public async Task Login_VerifyWithInvalidCredentials(LoginTestCase testCase)
+      [TestCaseSource(nameof(GetInvalidCases))]
+      public async Task Login_VerifyWithInvalidCredentials_ShouldFail(LoginTestCase testCase)
       {
-         // Skipping a test case with type of "Locked Out User".
-         if (testCase.Type == "locked_out_user")
+         var data = testCase.Data;
+
+         if (data == null)
          {
-            Util_ReportManager.Log(ReportInfo, $"Skipping test case {testCase.Id} of type {testCase.Type}");
-            Assert.Ignore($"Skipping test case {testCase.Id} of type {testCase.Type}");
+            Assert.Fail("Test data is null.");
+            return;
          }
 
          Util_ReportManager.Log(ReportInfo, "Entering Username.");
-         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, testCase.Data!.Username);
+         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, data.Username);
          Util_ReportManager.Log(ReportInfo, "Entering Password.");
-         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, testCase.Data!.Password);
+         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, data.Password);
          Util_ReportManager.Log(ReportInfo, "Clicking 'Login' button.");
          await _login.ClickElementAsync(Field_Login.LOGIN_BUTTON);
          Util_ReportManager.Log(ReportInfo, "Verifying that the user cannot login with invalid/missing credentials.");
          await Expect(_login.IsElementDisplayed(Field_Login.LOGIN_ERROR_MESSAGE)).ToBeVisibleAsync();
       }
 
-      [TestCaseSource(typeof(Provider_Login), nameof(Provider_Login.GetNegativeCases))]
-      public async Task Login_VerifyLockedUserAccount(LoginTestCase testCase)
+      [TestCaseSource(nameof(GetLockedOutUsers))]
+      public async Task Login_VerifyLockedUserAccount_ShouldFail(LoginTestCase testCase)
       {
-         // Skipping a test cases that are not with type of "Locked Out User".
-         if (testCase.Type != "locked_out_user")
+         var data = testCase.Data;
+
+         if (data == null)
          {
-            Util_ReportManager.Log(ReportInfo, $"Skipping test case {testCase.Id} of type {testCase.Type}");
-            Assert.Ignore($"Skipping test case {testCase.Id} of type {testCase.Type}");
+            Assert.Fail("Test data is null.");
+            return;
          }
 
          Util_ReportManager.Log(ReportInfo, "Entering Username.");
-         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, testCase.Data!.Username);
+         await _login.EnterTextAsync(Field_Login.LOGIN_USERNAME, data.Username);
          Util_ReportManager.Log(ReportInfo, "Entering Password.");
-         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, testCase.Data!.Password);
+         await _login.EnterTextAsync(Field_Login.LOGIN_PASSWORD, data.Password);
          Util_ReportManager.Log(ReportInfo, "Clicking 'Login' button.");
          await _login.ClickElementAsync(Field_Login.LOGIN_BUTTON);
          Util_ReportManager.Log(ReportInfo, "Verifying that the user cannot login with invalid/missing credentials.");
