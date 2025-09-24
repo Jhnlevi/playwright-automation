4040f8bf9a2c733da02c89b71a55cf301854b773
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-24 17:31:38 +0800

    feat: implement actions for menu component

--- Playwright.SauceDemo/Pages/Components/MenuComponent.cs
+++ Playwright.SauceDemo/Pages/Components/MenuComponent.cs
@@ -6,14 +6,14 @@ namespace Playwright.SauceDemo.Pages.Components
    internal class MenuComponent
    {
       private readonly IPage _page;
-      private readonly Dictionary<string, ILocator> _elements;
+      private readonly Dictionary<string, ILocator> _menuElements;
 
       // Constructor
       public MenuComponent(IPage page)
       {
          _page = page;
 
-         _elements = new Dictionary<string, ILocator>
+         _menuElements = new Dictionary<string, ILocator>
          {
             { MenuComponentConstants.MENU_ALL_ITEMS, _page.GetByRole(AriaRole.Link, new() { Name = "All Items" } ) },
             { MenuComponentConstants.MENU_ABOUT, _page.GetByRole(AriaRole.Link, new() { Name = "About" } ) },
@@ -22,5 +22,10 @@ namespace Playwright.SauceDemo.Pages.Components
             { MenuComponentConstants.MENU_CLOSE, _page.GetByRole(AriaRole.Button, new() { Name = "Close Menu" }) }
          };
       }
+
+      // Actions
+      public async Task ClickElementAsync(string field) => await _menuElements[field].ClickAsync();
+      public async Task<string> GetTextAsync(string field) => await _menuElements[field].InnerTextAsync();
+      public ILocator IsElementDisplayed(string field) => _menuElements[field];
    }
 }
