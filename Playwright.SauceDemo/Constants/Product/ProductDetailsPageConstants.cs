baccf9318dce5481c2eac2322f6373cc31f3dfdd
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-24 17:18:59 +0800

    feat: add a class file for product details page constants

---
+++ Playwright.SauceDemo/Constants/Product/ProductDetailsPageConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playwright.SauceDemo.Constants.Product
+{
+   internal class ProductDetailsPageConstants
+   {
+   }
+}
