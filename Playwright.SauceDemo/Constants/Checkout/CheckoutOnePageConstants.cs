cebcdc1fa35b63b92a5dd5b92d55478b214b7d77
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-24 18:49:29 +0800

    feat: add constant field for error message

--- Playwright.SauceDemo/Constants/Checkout/CheckoutOnePageConstants.cs
+++ Playwright.SauceDemo/Constants/Checkout/CheckoutOnePageConstants.cs
@@ -7,5 +7,6 @@
       public const string CHECKOUT_ONE_POSTAL = "checkout-one-postal";
       public const string CHECKOUT_ONE_CANCEL_BUTTON = "checkout-one-cancel-button";
       public const string CHECKOUT_ONE_CONTINUE_BUTTON = "checkout-one-continue-button";
+      public const string CHECKOUT_ONE_ERROR_MESSAGE = "checout-one-error-message";
    }
 }
