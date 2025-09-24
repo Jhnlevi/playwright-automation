5637c3bb2f5ec43594ebcf6c393d37671a857981
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-24 18:13:02 +0800

    feat: implement locators and actions for cart page

--- Playwright.SauceDemo/Pages/Cart/CartPage.cs
+++ Playwright.SauceDemo/Pages/Cart/CartPage.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Playwright.SauceDemo.Constants.Cart;
 
 namespace Playwright.SauceDemo.Pages.Cart
 {
    internal class CartPage
    {
+      private readonly IPage _page;
+      private readonly Dictionary<string, ILocator> _cartElements;
+
+      // Constructor
+      public CartPage(IPage page)
+      {
+         _page = page;
+         _cartElements = new Dictionary<string, ILocator>
+         {
+            { CartPageConstants.CART_ITEM, _page.Locator("div.cart_item") },
+            { CartPageConstants.CART_ITEM_QUANTITY, _page.Locator("div.cart_quantity") },
+            { CartPageConstants.CART_ITEM_REMOVE_BUTTON, _page.GetByRole(AriaRole.Button, new() { Name = "REMOVE" }) },
+            { CartPageConstants.CART_ITEM_CONTINUE_SHOPPING_BUTTON, _page.GetByRole(AriaRole.Link, new() { Name = "Continue Shopping" }) },
+            { CartPageConstants.CART_ITEM_CHECKOUT_BUTTON, _page.GetByRole(AriaRole.Link, new() { Name = "CHECKOUT" }) },
+         };
+      }
+
+      // Actions
+      public async Task ClickCartItemName(string field, string cartItem)
+      {
+         var item = _cartElements[field].Filter(new() { HasText = cartItem });
+         await item.GetByRole(AriaRole.Link, new() { Name = cartItem }).ClickAsync();
+      }
+
+      public async Task ClickRemoveCartItem(string field, string cartItem)
+      {
+         var item = _cartElements[field].Filter(new() { HasText = cartItem });
+         await item.GetByRole(AriaRole.Button, new() { Name = "REMOVE" }).ClickAsync();
+      }
+
+      public async Task<string> GetCartItemName(string field, string cartItem)
+      {
+         var item = _cartElements[field].Filter(new() { HasText = cartItem });
+         return await item.InnerTextAsync();
+      }
+
+      public async Task ClickElementAsync(string field) => await _cartElements[field].ClickAsync();
+
+      public ILocator IsElementDisplayed(string field) => _cartElements[field];
    }
 }
