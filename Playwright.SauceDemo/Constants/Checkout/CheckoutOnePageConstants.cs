94688b400e1412647f2b59dfdbe589245a25012b
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-24 18:40:31 +0800

    refactor: rename constant class files for checkout page one and two

---
+++ Playwright.SauceDemo/Constants/Checkout/CheckoutOnePageConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Playwright.SauceDemo.Constants.Checkout
+{
+   internal class CheckoutOnePageConstants
+   {
+      public const string CHECKOUT_ONE_FIRSTNAME = "checkout-one-firstname";
+      public const string CHECKOUT_ONE_LASTNAME = "checkout-one-lastname";
+      public const string CHECKOUT_ONE_POSTAL = "checkout-one-postal";
+      public const string CHECKOUT_ONE_CANCEL_BUTTON = "checkout-one-cancel-button";
+      public const string CHECKOUT_ONE_CONTINUE_BUTTON = "checkout-one-continue-button";
+   }
+}
--- Playwright.SauceDemo/Constants/Checkout/CheckoutTwoPageConstants.cs
+++ Playwright.SauceDemo/Constants/Checkout/CheckoutTwoPageConstants.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Playwright.SauceDemo.Constants.Checkout
 {
-   internal class CheckoutPageOneConstants
+   internal class CheckoutTwoPageConstants
    {
    }
 }
