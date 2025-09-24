4a575f227bbc7fee3a1f97272712a860b8c45aba
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-24 17:31:22 +0800

    feat: implement locators and actions for product details page

--- Playwright.SauceDemo/Pages/Product/ProductDetailsPage.cs
+++ Playwright.SauceDemo/Pages/Product/ProductDetailsPage.cs
@@ -1,13 +1,31 @@
-﻿namespace Playwright.SauceDemo.Pages.Product
+﻿using Microsoft.Playwright;
+using Playwright.SauceDemo.Constants.Product;
+
+namespace Playwright.SauceDemo.Pages.Product
 {
    internal class ProductDetailsPage
    {
-      // Constructor
-      public ProductDetailsPage()
-      {
+      private readonly IPage _page;
+      private readonly Dictionary<string, ILocator> _prodDetailsElements;
 
+      // Constructor
+      public ProductDetailsPage(IPage page)
+      {
+         _page = page;
+         _prodDetailsElements = new Dictionary<string, ILocator>
+         {
+            { ProductDetailsPageConstants.PRODUCT_DETAILS_DESC_CONTAINER, _page.Locator("div.inventory_details_desc_container") },
+            { ProductDetailsPageConstants.PRODUCT_DETAILS_DESC_NAME, _page.Locator("div.inventory_details_name") },
+            { ProductDetailsPageConstants.PRODUCT_DETAILS_DESC_DESCRIPTION, _page.Locator("div.inventory_details_desc")},
+            { ProductDetailsPageConstants.PRODUCT_DETAILS_ADD_TO_CART_BUTTON, _page.GetByRole(AriaRole.Button, new() { Name = "ADD TO CART" }) },
+            { ProductDetailsPageConstants.PRODUCT_DETAILS_REMOVE_FROM_CART_BUTTON, _page.GetByRole(AriaRole.Button, new() { Name = "REMOVE" }) },
+            { ProductDetailsPageConstants.PRODUCT_DETAILS_REMOVE_FROM_CART_BUTTON, _page.GetByRole(AriaRole.Button, new() { Name = "<- Back" }) }
+         };
       }
 
       //Actions
+      public async Task ClickElementAsync(string field) => await _prodDetailsElements[field].ClickAsync();
+      public async Task<string> GetTextAsync(string field) => await _prodDetailsElements[field].InnerTextAsync();
+      public ILocator IsElementDisplayed(string field) => _prodDetailsElements[field];
    }
 }
