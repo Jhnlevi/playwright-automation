bc49950cc02b56f2ef525203d30b13cc9f389016
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-25 21:45:47 +0800

    feat: implement new methods for getting test data for product sorters

--- Playwright.SauceDemo/Utils/Providers/ProductProvider.cs
+++ Playwright.SauceDemo/Utils/Providers/ProductProvider.cs
@@ -7,14 +7,26 @@ namespace Playwright.SauceDemo.Utils.Providers
       // Constants:
       private const string moduleName = "Product";
       private const string productsFile = "ProductTestData_Products.json";
+      private const string sortersFile = "ProductTestData_Sorters.json";
 
       /// <summary>
       /// Get single test case by Id or Type.
       /// </summary>
       /// <returns>The test case with matching Id or Type or both.</returns>
-      public static ProductTestCase? GetRecord(string fileName, string id)
+      public static ProductTestCase? GetProductRecord(string fileName, string id)
       {
          var data = JsonHelper.LoadJson<ProductTestData_Products>(moduleName, fileName);
+
+         return data?.TestCases?.FirstOrDefault(tc => tc.Id == id);
+      }
+
+      /// <summary>
+      /// Get single test case by Id or Type.
+      /// </summary>
+      /// <returns>The test case with matching Id or Type or both.</returns>
+      public static ProductSortersTestCase? GetSortRecord(string fileName, string id)
+      {
+         var data = JsonHelper.LoadJson<ProductTestData_Sorters>(moduleName, fileName);
 
          return data?.TestCases?.FirstOrDefault(tc => tc.Id == id);
       }
@@ -23,16 +35,28 @@ namespace Playwright.SauceDemo.Utils.Providers
       /// Get all test case
       /// </summary>
       /// <returns>All test cases.</returns>
-      public static IEnumerable<ProductTestCase> GetRecords(string fileName)
+      public static IEnumerable<ProductTestCase> GetProducts(string fileName)
       {
          var data = JsonHelper.LoadJson<ProductTestData_Products>(moduleName, fileName);
          return data?.TestCases ?? Enumerable.Empty<ProductTestCase>();
       }
 
+      /// <summary>
+      /// Get all test case
+      /// </summary>
+      /// <returns>All test cases.</returns>
+      public static IEnumerable<ProductSortersTestCase> GetSorters(string fileName)
+      {
+         var data = JsonHelper.LoadJson<ProductTestData_Sorters>(moduleName, fileName);
+         return data?.TestCases ?? Enumerable.Empty<ProductSortersTestCase>();
+      }
+
       // Get single record.
-      public static ProductTestCase? GetPositiveCaseById(string id) => GetRecord(fileName: productsFile, id);
+      public static ProductTestCase? GetProductById(string id) => GetProductRecord(fileName: productsFile, id);
+      public static ProductSortersTestCase? GetSortById(string id) => GetSortRecord(fileName: sortersFile, id);
 
       // Get multiple records.
-      public static IEnumerable<ProductTestCase> GetPositiveCases() => GetRecords(fileName: productsFile);
+      public static IEnumerable<ProductTestCase> GetAllProducts() => GetProducts(fileName: productsFile);
+      public static IEnumerable<ProductSortersTestCase> GetAllSorters() => GetSorters(fileName: sortersFile);
    }
 }
