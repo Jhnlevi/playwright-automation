30d1b93ff8b3fea5b51d72816c7119c0a65a3e03
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-27 22:09:54 +0800

    feat: implement methods for retrieving test data in 'CheckoutOneProvider'

--- Playwright.SauceDemo/Utils/Providers/CheckoutOneProvider.cs
+++ Playwright.SauceDemo/Utils/Providers/CheckoutOneProvider.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Playwright.SauceDemo.Models.Checkout;
 
 namespace Playwright.SauceDemo.Utils.Providers
 {
    internal class CheckoutOneProvider
    {
+      // Constants:
+      private const string moduleName = "Checkout";
+      private const string checkoutOnePositive = "CheckoutOneTestData_Positive.json";
+      private const string checkoutOneNegative = "CheckoutOneTestData_Negative.json";
+
+      /// <summary>
+      /// Get single test case by Id or Type.
+      /// </summary>
+      /// <returns>The test case with matching Id or Type or both.</returns>
+      public static CheckoutOneTestCase? GetRecord(string fileName, string id, string? type = null)
+      {
+         var data = JsonHelper.LoadJson<CheckoutOneTestData>(moduleName, fileName);
+
+         return data?.TestCases?
+            .FirstOrDefault(tc => tc.Id == id && (string.IsNullOrEmpty(type) || tc.Type == type));
+      }
+
+      /// <summary>
+      /// Get all test case
+      /// </summary>
+      /// <returns>All test cases.</returns>
+      public static IEnumerable<CheckoutOneTestCase> GetRecords(string fileName)
+      {
+         var data = JsonHelper.LoadJson<CheckoutOneTestData>(moduleName, fileName);
+         return data?.TestCases ?? Enumerable.Empty<CheckoutOneTestCase>();
+      }
+
+      // Get single record.
+      public static CheckoutOneTestCase? GetPositiveCaseById(string id) => GetRecord(fileName: checkoutOnePositive, id);
+      public static CheckoutOneTestCase? GetPositiveCaseByIdAndType(string id, string type) => GetRecord(fileName: checkoutOnePositive, id, type);
+      public static CheckoutOneTestCase? GetNegativeCaseById(string id) => GetRecord(fileName: checkoutOneNegative, id);
+      public static CheckoutOneTestCase? GetNegativeCaseByIdAndType(string id, string type) => GetRecord(fileName: checkoutOneNegative, id, type);
+
+      // Get multiple records.
+      public static IEnumerable<CheckoutOneTestCase> GetPositiveCases() => GetRecords(fileName: checkoutOnePositive);
+      public static IEnumerable<CheckoutOneTestCase> GetNegativeCases() => GetRecords(fileName: checkoutOneNegative);
    }
 }
