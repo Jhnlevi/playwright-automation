80bb138a6a87f341ec1b34405b0b7bdc74fffea2
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-28 15:30:15 +0800

    feat: implement locators and actions in checkout complete page class

--- Playwright.SauceDemo/Pages/Checkout/CheckoutCompletePage.cs
+++ Playwright.SauceDemo/Pages/Checkout/CheckoutCompletePage.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Playwright.SauceDemo.Constants.Checkout;
+using Playwright.SauceDemo.Pages.Components;
 
 namespace Playwright.SauceDemo.Pages.Checkout
 {
    internal class CheckoutCompletePage
    {
+      private readonly IPage _page;
+
+      // Dictionary field
+      private readonly Dictionary<string, ILocator> _checkoutCompleteElements;
+
+      // Component fields
+      public HeaderComponent _header { get; }
+      public FooterComponent _footer { get; }
+      public MenuComponent _menu { get; }
+
+      // Constructor
+      public CheckoutCompletePage(IPage page)
+      {
+         _page = page;
+
+         // Components
+         _header = new HeaderComponent(page);
+         _footer = new FooterComponent(page);
+         _menu = new MenuComponent(page);
+
+         _checkoutCompleteElements = new Dictionary<string, ILocator>
+         {
+            { CheckoutCompletePageConstants.CHECKOUT_COMPLETE_CONTAINER, _page.Locator("#checkout_complete_container") }
+         };
+      }
+
+      // Actions
+      public async Task<string> GetTextAsync(string field) => await _checkoutCompleteElements[field].InnerTextAsync();
+
+      public async Task ClickElementAsync(string field) => await _checkoutCompleteElements[field].ClickAsync();
+
+      public ILocator IsElementDisplayed(string field) => _checkoutCompleteElements[field];
    }
 }
