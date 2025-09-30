fb98cff587a9d86376ecad0f94f204f388aebf8b
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-30 22:13:04 +0800

    feat: implement constant fields for login page

--- Playwright.Parabank/Constants/Login/LoginPageConstants.cs
+++ Playwright.Parabank/Constants/Login/LoginPageConstants.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.Parabank.Constants.Login
+﻿namespace Playwright.Parabank.Constants.Login
 {
    internal class LoginPageConstants
    {
+      public const string LOGIN_USERNAME_FIELD = "login-username-field";
+      public const string LOGIN_PASSWORD_FIELD = "login-password-field";
+      public const string LOGIN_BUTTON = "login-button";
+      public const string LOGIN_FORGOT_LOGIN_INFO = "login-forgot-login-info";
+      public const string LOGIN_REGISTER_BUTTON = "login-register-button";
+      public const string LOGIN_LEFT_PANEL = "login-left-panel";
+      public const string LOGIN_RIGHT_PANEL = "login-right-panel";
    }
 }
