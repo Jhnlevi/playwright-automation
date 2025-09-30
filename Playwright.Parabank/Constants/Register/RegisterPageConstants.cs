b1f828cc2a00a3968c5006ab995f7d7dcd36b475
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-30 22:23:57 +0800

    feat: implement constant fields for register page

--- Playwright.Parabank/Constants/Register/RegisterPageConstants.cs
+++ Playwright.Parabank/Constants/Register/RegisterPageConstants.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.Parabank.Constants.Register
+﻿namespace Playwright.Parabank.Constants.Register
 {
    internal class RegisterPageConstants
    {
+      public const string REGISTER_FIRST_NAME_FIELD = "register-first-name-field";
+      public const string REGISTER_LAST_NAME_FIELD = "register-last-name-field";
+      public const string REGISTER_ADDRESS_FIELD = "register-address-field";
+      public const string REGISTER_CITY_FIELD = "register-city-field";
+      public const string REGISTER_STATE_FIELD = "register-state-field";
+      public const string REGISTER_ZIP_CODE_FIELD = "register-zip-code-field";
+      public const string REGISTER_MOBILE_NUMBER_FIELD = "register-mobile-number-field";
+      public const string REGISTER_SSN_FIELD = "register-ssn-field";
+      public const string REGISTER_BUTTON = "register-button";
+      public const string REGISTER_USERNAME_FIELD = "register-username-field";
+      public const string REGISTER_PASSWORD_FIELD = "register-password-field";
+      public const string REGISTER_CONFIRM_PASSWORD_FIELD = "register-confirm-password-field";
+      public const string REGISTER_LEFT_PANEL = "register-left-panel";
+      public const string REGISTER_RIGHT_PANEL = "register-right-panel";
    }
 }
