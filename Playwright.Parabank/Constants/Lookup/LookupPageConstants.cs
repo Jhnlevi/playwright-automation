e033b74fbe49260381338f7265859cb4295be507
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-30 22:18:00 +0800

    feat: implement constant fields for lookup page

--- Playwright.Parabank/Constants/Lookup/LookupPageConstants.cs
+++ Playwright.Parabank/Constants/Lookup/LookupPageConstants.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.Parabank.Constants.Lookup
+﻿namespace Playwright.Parabank.Constants.Lookup
 {
    internal class LookupPageConstants
    {
+      public const string LOOKUP_FIRST_NAME_FIELD = "lookup-first-name-field";
+      public const string LOOKUP_LAST_NAME_FIELD = "lookup-last-name-field";
+      public const string LOOKUP_ADDRESS_FIELD = "lookup-address-field";
+      public const string LOOKUP_CITY_FIELD = "lookup-city-field";
+      public const string LOOKUP_STATE_FIELD = "lookup-state-field";
+      public const string LOOKUP_ZIP_CODE_FIELD = "lookup-zip-code-field";
+      public const string LOOKUP_SSN_FIELD = "lookup-ssn-field";
+      public const string LOOKUP_FIND_INFO_BUTTON = "lookup-find-info-button";
+      public const string LOOKUP_LEFT_PANEL = "lookup-left-panel";
+      public const string LOOKUP_RIGHT_PANEL = "lookup-right-panel";
    }
 }
