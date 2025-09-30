ea911736ba894792cac410333c55265640d1cfc0
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-30 22:03:57 +0800

    feat: implement methods for loading .json contents

--- Playwright.Parabank/Utils/JsonHelper.cs
+++ Playwright.Parabank/Utils/JsonHelper.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 
 namespace Playwright.Parabank.Utils
 {
    internal class JsonHelper
    {
+      /// <summary>
+      /// Reads the .json file
+      /// </summary>
+      /// <returns>Deserialized json file.</returns>
+      private static T ReadJson<T>(string path)
+      {
+         var data = File.ReadAllText(path);
+         return JsonSerializer.Deserialize<T>(data)!;
+      }
+
+      public static T LoadJson<T>(string module = "", string file = "")
+      {
+         var directory = string.IsNullOrWhiteSpace(module)
+            ? PathsHelper.GetTestDataPath()
+            : Path.Combine(PathsHelper.GetTestDataPath(), module);
+
+         if (!Directory.Exists(directory))
+            throw new DirectoryNotFoundException("Test data module folder not found.");
+
+         if (string.IsNullOrWhiteSpace(file))
+            throw new ArgumentException("File name must be provided to load test data.");
+
+         var filePath = Path.Combine(directory, file);
+
+         if (!File.Exists(filePath))
+            throw new FileNotFoundException($"{file} is not found at {directory}");
+
+         return ReadJson<T>(filePath);
+      }
    }
 }
