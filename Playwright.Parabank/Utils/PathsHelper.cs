27ef6d413355baf9e096eabed2fd91c74d69bed4
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-30 22:05:32 +0800

    feat: implement methods for getting project folder paths

--- Playwright.Parabank/Utils/PathsHelper.cs
+++ Playwright.Parabank/Utils/PathsHelper.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.Parabank.Utils
+﻿namespace Playwright.Parabank.Utils
 {
    internal class PathsHelper
    {
+      /// <summary>
+      /// Returns the appsettings path.
+      /// </summary>
+      public static string GetConfigPath() => GetProjectRoot();
+
+      /// <summary>
+      /// Returns the path of 'Reports' folder.
+      /// </summary>
+      public static string GetReportPath() => Path.Combine(GetProjectRoot(), "Reports");
+
+      /// <summary>
+      /// Returns the path of 'Screenshots' folder inside 'Reports'.
+      /// </summary>
+      /// <returns></returns>
+      public static string GetScreenshotPath() => Path.Combine(GetProjectRoot(), "Reports", "Screenshots");
+
+      public static string GetTestDataPath() => Path.Combine(GetProjectRoot(), "TestData");
+
+      private static string GetProjectRoot()
+      {
+         // First check: walk up the directories until csproj is found.
+         var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+         while (currentDirectory != null)
+         {
+            if (currentDirectory.GetFiles("*.csproj").Any())
+               return currentDirectory.FullName;
+
+            currentDirectory = currentDirectory.Parent;
+         }
+
+         // Second check: return current directory.
+         return Directory.GetCurrentDirectory();
+      }
    }
 }
