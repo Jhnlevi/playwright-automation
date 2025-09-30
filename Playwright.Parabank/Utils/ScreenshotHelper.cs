0929ddf15a1b825c741225e761687d82b8bedcd9
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-09-30 22:06:39 +0800

    feat: implement method for handling screenshots

--- Playwright.Parabank/Utils/ScreenshotHelper.cs
+++ Playwright.Parabank/Utils/ScreenshotHelper.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 
 namespace Playwright.Parabank.Utils
 {
    internal class ScreenshotHelper
    {
+      public static async Task<string> CaptureAsync(IPage page, string name = "")
+      {
+         var directory = PathsHelper.GetScreenshotPath();
+         Directory.CreateDirectory(directory);
+
+         var defaultName = String.IsNullOrWhiteSpace(name) ? "Screenshot" : name;
+         var fileName = $"{defaultName}_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.png";
+         var filePath = Path.Combine(directory, fileName);
+
+         await page.ScreenshotAsync(new PageScreenshotOptions
+         {
+            Path = filePath,
+            FullPage = true
+         });
+
+         return filePath;
+      }
    }
 }
