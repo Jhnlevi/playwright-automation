33cda015972b275ed59d9d020b07ba9432740c3e
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-02 17:42:12 +0800

    feat: add model fields for login test data

--- Playwright.Parabank/Models/Login/LoginModel.cs
+++ Playwright.Parabank/Models/Login/LoginModel.cs
@@ -1,6 +1,61 @@
-﻿namespace Playwright.Parabank.Models.Login
+﻿using System.Text.Json.Serialization;
+
+namespace Playwright.Parabank.Models.Login
 {
-   internal class LoginModel
+   public class LoginModel
    {
+      [JsonPropertyName("testCases")]
+      public List<LoginTestCase>? TestCases { get; set; }
+   }
+
+   public class LoginTestCase
+   {
+      [JsonPropertyName("id")]
+      public string Id { get; set; } = null!;
+
+      [JsonPropertyName("description")]
+      public string Description { get; set; } = null!;
+
+      [JsonPropertyName("type")]
+      public string? Type { get; set; }
+
+      [JsonPropertyName("data")]
+      public Data Data { get; set; } = null!;
+
+      [JsonPropertyName("expectedResult")]
+      public Expectedresult ExpectedResult { get; set; } = null!;
+   }
+
+   public class Data
+   {
+      [JsonPropertyName("user")]
+      public User User { get; set; } = null!;
+   }
+
+   public class User
+   {
+      [JsonPropertyName("userName")]
+      public string UserName { get; set; } = null!;
+
+      [JsonPropertyName("password")]
+      public string Password { get; set; } = null!;
+   }
+
+   public class Expectedresult
+   {
+      [JsonPropertyName("message")]
+      public string Message { get; set; } = null!;
+
+      [JsonPropertyName("fieldErrors")]
+      public List<Fielderrors>? FieldErrors { get; set; }
+   }
+
+   public class Fielderrors
+   {
+      [JsonPropertyName("field")]
+      public string Field { get; set; } = null!;
+
+      [JsonPropertyName("message")]
+      public string Message { get; set; } = null!;
    }
 }
