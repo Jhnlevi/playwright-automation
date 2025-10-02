05bd3d5c700d48ef7bb71f0e5fbe49f6b1b5d850
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-02 17:53:40 +0800

    feat: implement report, config, and test result setup/teardown

--- Playwright.Parabank/Tests/BaseTest.cs
+++ Playwright.Parabank/Tests/BaseTest.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright.NUnit;
+using Playwright.Parabank.Models;
+using Playwright.Parabank.Utils;
 
 namespace Playwright.Parabank.Tests
 {
-   internal class BaseTest
+   internal class BaseTest : PageTest
    {
+      protected AppSettings _config;
+
+      [OneTimeSetUp]
+      public void ReportSetup() => ReportManager.CreateExtentReport("Parabank");
+
+      [SetUp]
+      public virtual void Setup()
+      {
+         _config = ConfigHelper.Load<AppSettings>("appsettings.json", "ConfigSettings");
+
+         ReportManager.CreateExtentTest(TestContext.CurrentContext.Test.Name);
+      }
+
+      [TearDown]
+      public async Task Teardown()
+      {
+         var context = TestContext.CurrentContext;
+         var status = context.Result.Outcome.Status.ToString();
+         var message = context.Result.Message;
+         var trace = context.Result.StackTrace;
+         var name = context.Test.MethodName;
+
+         await ResultHelper.LogResultsAsync(Page, status, message, trace!, name!);
+      }
+
+      [OneTimeTearDown]
+      public void ReportClose() => ReportManager.QuitExtentReport();
    }
 }
