bb03f3fb18dbd1437c16a902c5b94ad6b796d32e
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-02 17:49:19 +0800

    feat: implement methods for getting register test data

--- Playwright.Parabank/Utils/Providers/RegisterProvider.cs
+++ Playwright.Parabank/Utils/Providers/RegisterProvider.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Playwright.Parabank.Models.Register;
 
 namespace Playwright.Parabank.Utils.Providers
 {
-   internal class RegisterProvider
+   internal static class RegisterProvider
    {
+      private const string moduleName = "Registers";
+      private const string positive = "RegisterTD_Positive.json";
+      private const string negative = "RegisterTD_Negative.json";
+
+      public static IEnumerable<RegisterTestCase> GetRecords(string fileName)
+      {
+         var data = JsonHelper.LoadJson<RegisterModel>(moduleName, fileName);
+         return data?.TestCases ?? Enumerable.Empty<RegisterTestCase>();
+      }
+
+      public static IEnumerable<RegisterTestCase> GetPositiveCases() => GetRecords(fileName: positive);
+      public static IEnumerable<RegisterTestCase> GetNegativeCases() => GetRecords(fileName: negative);
    }
 }
