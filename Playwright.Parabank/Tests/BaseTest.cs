4da274fde234071450948dbeb4b41cba3e7aa367
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-02 17:50:23 +0800

    feat: add 'Tests' folder and BaseTest class file

---
+++ Playwright.Parabank/Tests/BaseTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playwright.Parabank.Tests
+{
+   internal class BaseTest
+   {
+   }
+}
