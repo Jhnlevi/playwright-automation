9d55521e9fee4df2b9586422f529cee8ac4d9ffd
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-04 06:39:19 +0800

    feat: add constant field for new id

--- Playwright.Parabank/Constants/Protected/OpenNewAccountPageConstants.cs
+++ Playwright.Parabank/Constants/Protected/OpenNewAccountPageConstants.cs
@@ -6,7 +6,8 @@
       public const string ONA_ACCOUNT_RESULT = "ona-account-result";
       public const string ONA_ACCOUNT_ERROR = "ona-account-error";
       public const string ONA_ACCOUNT_TYPE = "ona-account-type";
-      public const string ONA_ACCOUNT_ID = "ona-account-id";
+      public const string ONA_ACCOUNT_EXISTING_ID = "ona-account-existing-id";
+      public const string ONA_ACCOUNT_NEW_ID = "ona-account-new-id";
       public const string ONA_BUTTON = "ona-button";
    }
 }
