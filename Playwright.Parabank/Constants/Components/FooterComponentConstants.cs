4663b227157fc4c0f0a15f016e54bd3feca90dc5
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-03 10:35:08 +0800

    feat: add constant fields for footer compoenent

--- Playwright.Parabank/Constants/Components/FooterComponentConstants.cs
+++ Playwright.Parabank/Constants/Components/FooterComponentConstants.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.Parabank.Constants.Components
+﻿namespace Playwright.Parabank.Constants.Components
 {
    internal class FooterComponentConstants
    {
+      public const string FOOTER_HOME = "footer-home";
+      public const string FOOTER_ABOUT = "footer-about";
+      public const string FOOTER_SERVICES = "footer-services";
+      public const string FOOTER_PRODUCTS = "footer-products";
+      public const string FOOTER_LOCATIONS = "footer-locations";
+      public const string FOOTER_FORUM = "footer-forums";
+      public const string FOOTER_SITE_MAP = "footer-site-map";
+      public const string FOOTER_CONTACT_US = "footer-contact-us";
    }
 }
