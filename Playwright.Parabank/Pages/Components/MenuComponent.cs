6c6ed31ccd251e28bb002e4fea85d18500d24c06
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-03 10:48:16 +0800

    feat: implement locators and action methods for menu component

--- Playwright.Parabank/Pages/Components/MenuComponent.cs
+++ Playwright.Parabank/Pages/Components/MenuComponent.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Playwright.Parabank.Constants.Components;
 
 namespace Playwright.Parabank.Pages.Components
 {
    internal class MenuComponent
    {
+      private readonly IPage _page;
+
+      private readonly Dictionary<string, ILocator> _menuElements;
+
+      public MenuComponent(IPage page)
+      {
+         _page = page;
+
+         _menuElements = new Dictionary<string, ILocator>
+         {
+            { MenuComponentConstants.MENU_LINK_OPEN_ACCOUNT, _page.GetByRole(AriaRole.Link, new() { Name = "Open New Account" })},
+            { MenuComponentConstants.MENU_LINK_OVERVIEW, _page.GetByRole(AriaRole.Link, new() { Name = "Accounts Overview" })},
+            { MenuComponentConstants.MENU_LINK_TRANSFER_FUNDS, _page.GetByRole(AriaRole.Link, new() { Name = "Transfer Funds" })},
+            { MenuComponentConstants.MENU_LINK_BILLS_PAY, _page.GetByRole(AriaRole.Link, new() { Name = "Bill Pay" })},
+            { MenuComponentConstants.MENU_LINK_FIND_TRANSACTIONS, _page.GetByRole(AriaRole.Link, new() { Name = "Find Transactions" })},
+            { MenuComponentConstants.MENU_LINK_UPDATE_CONTACT_INFO, _page.GetByRole(AriaRole.Link, new() { Name = "Update Contact Info" })},
+            { MenuComponentConstants.MENU_LINK_REQUEST_LOAN, _page.GetByRole(AriaRole.Link, new() { Name = "Request Loan" })},
+            { MenuComponentConstants.MENU_LINK_LOGOUT, _page.GetByRole(AriaRole.Link, new() { Name = "Log Out" })}
+         };
+      }
+
+      public async Task ClickElementAsync(string field) => await _menuElements[field].ClickAsync();
+
+      public ILocator IsElementDisplayed(string field) => _menuElements[field];
    }
 }
