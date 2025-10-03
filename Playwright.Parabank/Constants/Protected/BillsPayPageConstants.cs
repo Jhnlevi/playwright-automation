b946a0fff8d7d68679c85756d80e61514c8d45da
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-04 06:44:23 +0800

    feat: add constant fields for 'Bill Pay' page

--- Playwright.Parabank/Constants/Protected/BillsPayPageConstants.cs
+++ Playwright.Parabank/Constants/Protected/BillsPayPageConstants.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.Parabank.Constants.Protected
+﻿namespace Playwright.Parabank.Constants.Protected
 {
    internal class BillsPayPageConstants
    {
+      public const string BP_FORM = "bp-form";
+      public const string BP_RESULT = "bp-result";
+      public const string BP_ERROR = "bp-error";
+      public const string BP_PAYEE_NAME = "bp-payee-name";
+      public const string BP_ADDRESS = "bp-address";
+      public const string BP_CITY = "bp-city";
+      public const string BP_STATE = "bp-state";
+      public const string BP_ZIP_CODE = "bp-zip-code";
+      public const string BP_MOBILE_NUMBER = "bp-mobile-number";
+      public const string BP_ACCOUNT_NUMBER = "bp-account-number";
+      public const string BP_VERIFY_ACCOUNT_NUMBER = "bp-verify-account-number";
+      public const string BP_AMOUNT = "bp-amount";
+      public const string BP_FROM_ACCOUNT = "bp-from-account";
+      public const string BP_SEND_PAYMENT_BUTTON = "bp-send-payment-button";
    }
 }
