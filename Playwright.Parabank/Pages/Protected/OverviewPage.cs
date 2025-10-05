9deeca5c137712f0647c2a2de3701cfb856a543a
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-05 16:36:33 +0800

    feat: implement locators and action methods for overview page

--- Playwright.Parabank/Pages/Protected/OverviewPage.cs
+++ Playwright.Parabank/Pages/Protected/OverviewPage.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Playwright.Parabank.Constants.Overview;
 
 namespace Playwright.Parabank.Pages.Protected
 {
    internal class OverviewPage
    {
+      private readonly IPage _page;
+
+      private Dictionary<string, ILocator> _overviewElements;
+
+      public OverviewPage(IPage page)
+      {
+         _page = page;
+
+         _overviewElements = new Dictionary<string, ILocator>
+         {
+            { OverviewPageConstants.OVERVIEW_SHOW, _page.Locator("#showOverview") },
+            { OverviewPageConstants.OVERVIEW_ERROR, _page.Locator("#showError") },
+            { OverviewPageConstants.OVERVIEW_TABLE, _page.Locator("#accountTable") }
+         };
+      }
+
+      public async Task<string> GetTextAsync(string field) => await _overviewElements[field].InnerTextAsync();
+
+      public ILocator IsElementDisplayed(string field) => _overviewElements[field];
    }
 }
