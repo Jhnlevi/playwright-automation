dfcf13ab4d72a20b7ab8891c655a6d02549877da
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-07 02:42:44 +0800

    feat: implement test data mapping fields for ONAModel

--- Playwright.Parabank/Models/Protected/ONAModel.cs
+++ Playwright.Parabank/Models/Protected/ONAModel.cs
@@ -1,12 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Playwright.Parabank.Models.Protected
 {
    internal class ONAModel
    {
+      [JsonPropertyName("testCases")]
+      public List<ONATestCase>? TestCases { get; set; }
+   }
+
+   public class ONATestCase
+   {
+      [JsonPropertyName("id")]
+      public string Id { get; set; } = null!;
+
+      [JsonPropertyName("description")]
+      public string Description { get; set; } = null!;
+
+      [JsonPropertyName("type")]
+      public string Type { get; set; } = null!;
+
+      [JsonPropertyName("data")]
+      public Data Data { get; set; } = null!;
+
+      [JsonPropertyName("expectedResult")]
+      public Expectedresult ExpectedResult { get; set; } = null!;
+   }
+
+   public class Data
+   {
+      [JsonPropertyName("account")]
+      public Account Account { get; set; } = null!;
+   }
+
+   public class Account
+   {
+      [JsonPropertyName("accountType")]
+      public string AccountType { get; set; } = null!;
+
+      [JsonPropertyName("existingAccount")]
+      public string ExistingAccount { get; set; } = null!;
+   }
+
+   public class Expectedresult
+   {
+      [JsonPropertyName("message")]
+      public string Message { get; set; } = null!;
+
+      [JsonPropertyName("fieldErrors")]
+      public List<Fielderrors>? FieldErrors { get; set; } = null!;
+   }
+   public class Fielderrors
+   {
+      [JsonPropertyName("field")]
+      public string Field { get; set; } = null!;
+
+      [JsonPropertyName("message")]
+      public string Message { get; set; } = null!;
    }
 }
