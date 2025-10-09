9e1d3a6fea572c70a114911fe665a7f00d7801c2
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-10 03:10:04 +0800

    feat: implement methods for retrieving test data in 'TFProvider'

--- Playwright.Parabank/Utils/Providers/TFProvider.cs
+++ Playwright.Parabank/Utils/Providers/TFProvider.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Playwright.Parabank.Models.Protected;
 
 namespace Playwright.Parabank.Utils.Providers
 {
    internal class TFProvider
    {
+      private const string moduleName = "TransferFund";
+      private const string positive = "TFTD_Positive.json";
+      private const string negative = "TFTD_Negative.json";
+
+      public static IEnumerable<TFTestCase> GetRecords(string fileName)
+      {
+         var data = JsonHelper.LoadJson<TFModel>(moduleName, fileName);
+         return data?.TestCases ?? Enumerable.Empty<TFTestCase>();
+      }
+
+      public static IEnumerable<TFTestCase> GetPositiveCases() => GetRecords(fileName: positive);
+      public static IEnumerable<TFTestCase> GetNegativeCases() => GetRecords(fileName: negative);
    }
 }
