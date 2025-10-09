cdb0a0409c1e7e2dadcfe40dd7b02f9ba163da27
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-10 00:27:15 +0800

    feat: implement methods for retrieveing test data in 'ONAProvider'

--- Playwright.Parabank/Utils/Providers/ONAProvider.cs
+++ Playwright.Parabank/Utils/Providers/ONAProvider.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Playwright.Parabank.Models.Protected;
 
 namespace Playwright.Parabank.Utils.Providers
 {
    internal class ONAProvider
    {
+      private const string moduleName = "OpenNewAccount";
+      private const string positive = "ONATD_Positive.json";
+
+      public static IEnumerable<ONATestCase> GetRecords(string fileName)
+      {
+         var data = JsonHelper.LoadJson<ONAModel>(moduleName, fileName);
+         return data?.TestCases ?? Enumerable.Empty<ONATestCase>();
+      }
+
+      public static IEnumerable<ONATestCase> GetPositiveCases() => GetRecords(fileName: positive);
    }
 }
