1c809114d2e87cac7d7c05acfd166c7f04ce7090
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-10 03:05:57 +0800

    feat: add accountId for sharing between modules

--- Playwright.Parabank/Tests/UI/Public/LoginTests.cs
+++ Playwright.Parabank/Tests/UI/Public/LoginTests.cs
@@ -6,81 +6,85 @@ using Playwright.Parabank.Utils.Providers;
 
 namespace Playwright.Parabank.Tests.UI.Public
 {
-    internal class LoginTests : BaseTest
-    {
-        private readonly ReportManager.LogLevel _info = ReportManager.LogLevel.Info;
-        private LoginPage _login;
+   internal class LoginTests : BaseTest
+   {
+      private readonly ReportManager.LogLevel _info = ReportManager.LogLevel.Info;
+      private LoginPage _login;
 
-        [SetUp]
-        public override void Setup()
-        {
-            base.Setup();
+      [SetUp]
+      public override void Setup()
+      {
+         base.Setup();
 
-            _login = new LoginPage(Page);
+         _login = new LoginPage(Page);
 
-            var URL = _config.Environments.Qa.BaseUrl;
+         var URL = _config.Environments.Qa.BaseUrl;
 
-            ReportManager.Log(_info, "Navigating to Parabank Website.");
-            Page.GotoAsync(URL + LoginPageConstants.URL_PATH);
-        }
+         ReportManager.Log(_info, "Navigating to Parabank Website.");
+         Page.GotoAsync(URL + LoginPageConstants.URL_PATH);
+      }
 
-        [Category("UI")]
-        [TestCaseSource(typeof(LoginProvider), nameof(LoginProvider.GetPositiveCases))]
-        public async Task Login_WtihValidCredentials_ShouldSucceed(LoginTestCase testCase)
-        {
-            var data = testCase.Data;
-            Assert.That(data, Is.Not.Null, "Data should not be null.");
+      [Category("UI")]
+      [TestCaseSource(typeof(LoginProvider), nameof(LoginProvider.GetPositiveCases))]
+      public async Task Login_WtihValidCredentials_ShouldSucceed(LoginTestCase testCase)
+      {
+         var data = testCase.Data;
+         Assert.That(data, Is.Not.Null, "Data should not be null.");
 
-            var user = data.User;
-            Assert.That(user, Is.Not.Null, "User should not be null.");
+         var user = data.User;
+         Assert.That(user, Is.Not.Null, "User should not be null.");
 
-            ReportManager.Log(_info, "Entering username");
-            await _login.EnterTextAsync(LoginPageConstants.LOGIN_USERNAME_FIELD, user.UserName);
-            ReportManager.Log(_info, "Entering password");
-            await _login.EnterTextAsync(LoginPageConstants.LOGIN_PASSWORD_FIELD, user.Password);
-            ReportManager.Log(_info, "Clicking 'Login' button.");
-            await _login.ClickElementAsync(LoginPageConstants.LOGIN_BUTTON);
-            ReportManager.Log(_info, "Verifying that the user reaches the dashboard overview after logging in.");
+         ReportManager.Log(_info, "Entering username");
+         await _login.EnterTextAsync(LoginPageConstants.LOGIN_USERNAME_FIELD, user.UserName);
+         ReportManager.Log(_info, "Entering password");
+         await _login.EnterTextAsync(LoginPageConstants.LOGIN_PASSWORD_FIELD, user.Password);
+         ReportManager.Log(_info, "Clicking 'Login' button.");
+         await _login.ClickElementAsync(LoginPageConstants.LOGIN_BUTTON);
+         ReportManager.Log(_info, "Verifying that the user reaches the dashboard overview after logging in.");
 
-            var overview = Page.Locator("#showOverview");
+         var accountId = await Page.Locator("a[href^='activity.htm?id=']").InnerTextAsync();
 
-            Assert.That(Page.Url, Does.Contain("parabank/overview"));
-            await Expect(overview).ToBeVisibleAsync();
-        }
+         CustomTestContext.AccountId = accountId;
 
-        [TestCaseSource(typeof(CustomDataSource), nameof(CustomDataSource.RemoveTC05))]
-        public async Task Login_WithInvalidCredentials_ShouldFail(LoginTestCase testCase)
-        {
-            var data = testCase.Data;
-            Assert.That(data, Is.Not.Null, "Data should not be null.");
+         var overview = Page.Locator("#showOverview");
 
-            var user = data.User;
-            Assert.That(user, Is.Not.Null, "User should not be null.");
+         Assert.That(Page.Url, Does.Contain("parabank/overview"));
+         await Expect(overview).ToBeVisibleAsync();
+      }
 
-            ReportManager.Log(_info, "Entering username");
-            await _login.EnterTextAsync(LoginPageConstants.LOGIN_USERNAME_FIELD, user.UserName);
-            ReportManager.Log(_info, "Entering password");
-            await _login.EnterTextAsync(LoginPageConstants.LOGIN_PASSWORD_FIELD, user.Password);
-            ReportManager.Log(_info, "Clicking 'Login' button.");
-            await _login.ClickElementAsync(LoginPageConstants.LOGIN_BUTTON);
-            ReportManager.Log(_info, "Verifying that login process will not continue with invalid credentials.");
+      [TestCaseSource(typeof(CustomDataSource), nameof(CustomDataSource.RemoveTC05))]
+      public async Task Login_WithInvalidCredentials_ShouldFail(LoginTestCase testCase)
+      {
+         var data = testCase.Data;
+         Assert.That(data, Is.Not.Null, "Data should not be null.");
 
-            var fieldErrors = testCase.ExpectedResult.FieldErrors;
-            Assert.That(fieldErrors, Is.Not.Null, "Field errors should not be null.");
+         var user = data.User;
+         Assert.That(user, Is.Not.Null, "User should not be null.");
 
-            foreach (var err in fieldErrors)
-            {
+         ReportManager.Log(_info, "Entering username");
+         await _login.EnterTextAsync(LoginPageConstants.LOGIN_USERNAME_FIELD, user.UserName);
+         ReportManager.Log(_info, "Entering password");
+         await _login.EnterTextAsync(LoginPageConstants.LOGIN_PASSWORD_FIELD, user.Password);
+         ReportManager.Log(_info, "Clicking 'Login' button.");
+         await _login.ClickElementAsync(LoginPageConstants.LOGIN_BUTTON);
+         ReportManager.Log(_info, "Verifying that login process will not continue with invalid credentials.");
 
-                var error = await Page.Locator($".{err.Field}").InnerTextAsync();
-                ReportManager.Log(_info, $"Error message: {error}");
-                Assert.That(error, Is.EqualTo($"{err.Message}"));
-            }
-        }
+         var fieldErrors = testCase.ExpectedResult.FieldErrors;
+         Assert.That(fieldErrors, Is.Not.Null, "Field errors should not be null.");
 
-        // Filtered test cases.
-        private static class CustomDataSource
-        {
-            public static IEnumerable<LoginTestCase> RemoveTC05() => LoginProvider.GetNegativeCases().Where(tc => tc.Id != "TC_LOGIN_0005");
-        }
-    }
+         foreach (var err in fieldErrors)
+         {
+
+            var error = await Page.Locator($".{err.Field}").InnerTextAsync();
+            ReportManager.Log(_info, $"Error message: {error}");
+            Assert.That(error, Is.EqualTo($"{err.Message}"));
+         }
+      }
+
+      // Filtered test cases.
+      private static class CustomDataSource
+      {
+         public static IEnumerable<LoginTestCase> RemoveTC05() => LoginProvider.GetNegativeCases().Where(tc => tc.Id != "TC_LOGIN_0005");
+      }
+   }
 }
