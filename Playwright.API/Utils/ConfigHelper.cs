cd337eb2e403d1be0876af56db70eed36c4f690d
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-12 04:04:22 +0800

    feat: implement methods for loading appsettings data inn 'ConfigHelper' class

--- Playwright.API/Utils/ConfigHelper.cs
+++ Playwright.API/Utils/ConfigHelper.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace Playwright.API.Utils
 {
-   internal class ConfigHelper
+   internal static class ConfigHelper
    {
+      public static T Load<T>(string? fileName = "appsettings.json")
+      {
+         var config = new ConfigurationBuilder()
+             .SetBasePath(ProjectPathHelper.GetConfigPath())
+             .AddJsonFile(fileName!, optional: false, reloadOnChange: true)
+             .AddEnvironmentVariables()
+             .Build();
+
+         return config.Get<T>()!;
+      }
+      public static T Load<T>(string? fileName = "appsettings.json", string? sectionName = "")
+      {
+         var config = new ConfigurationBuilder()
+             .SetBasePath(ProjectPathHelper.GetConfigPath())
+             .AddJsonFile(fileName!, optional: false, reloadOnChange: true)
+             .AddEnvironmentVariables()
+             .Build();
+
+         return config.GetSection(sectionName!).Get<T>()!;
+      }
    }
 }
