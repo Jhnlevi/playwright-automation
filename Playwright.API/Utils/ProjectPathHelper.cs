7cc9307dfe669e7d830a156f372a1af40a76e65b
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-12 04:01:57 +0800

    feat: implement methods in ProjectPathHelper class for retrieving folder paths

--- Playwright.API/Utils/ProjectPathHelper.cs
+++ Playwright.API/Utils/ProjectPathHelper.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Playwright.API.Utils
+﻿namespace Playwright.API.Utils
 {
-   internal class ProjectPathHelper
+   internal static class ProjectPathHelper
    {
+      public static string GetConfigPath() => GetProjectRoot();
+
+      public static string GetReportPath() => Path.Combine(GetProjectRoot(), "Reports");
+
+      private static string GetProjectRoot()
+      {
+         var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+         while (currentDirectory != null)
+         {
+            if (currentDirectory.GetFiles("*.csproj").Any())
+               return currentDirectory.FullName;
+
+            currentDirectory = currentDirectory.Parent;
+         }
+         return Directory.GetCurrentDirectory();
+      }
    }
 }
