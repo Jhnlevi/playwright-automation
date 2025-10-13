501c373eff0909d1c5a08391c6d532e518cd57d3
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-13 14:14:35 +0800

    refactor: comment optional fields

--- Playwright.API/Models/Post/PostModel.cs
+++ Playwright.API/Models/Post/PostModel.cs
@@ -4,20 +4,20 @@ namespace Playwright.API.Models.Post
 {
    internal class PostModel
    {
-      [JsonPropertyName("postId")]
-      public int? PostId { get; set; }
+      //[JsonPropertyName("postId")]
+      //public int? PostId { get; set; }
 
       [JsonPropertyName("userId")]
-      public int? UserId { get; set; }
+      public int UserId { get; set; }
 
       [JsonPropertyName("id")]
-      public int Id { get; set; }
+      public int? Id { get; set; }
 
-      [JsonPropertyName("name")]
-      public string? Name { get; set; }
+      //[JsonPropertyName("name")]
+      //public string? Name { get; set; }
 
-      [JsonPropertyName("email")]
-      public string? Email { get; set; }
+      //[JsonPropertyName("email")]
+      //public string? Email { get; set; }
 
       [JsonPropertyName("title")]
       public string Title { get; set; } = null!;
