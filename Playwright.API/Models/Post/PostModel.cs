dd40d7f94208e6aaad3bd7218fdde680a604b2d7
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-13 11:27:15 +0800

    feat: implement json field mappings in PostModel

--- Playwright.API/Models/Post/PostModel.cs
+++ Playwright.API/Models/Post/PostModel.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Playwright.API.Models.Post
 {
    internal class PostModel
    {
+      [JsonPropertyName("postId")]
+      public int? PostId { get; set; }
+
+      [JsonPropertyName("userId")]
+      public int? UserId { get; set; }
+
+      [JsonPropertyName("id")]
+      public int Id { get; set; }
+
+      [JsonPropertyName("name")]
+      public string? Name { get; set; }
+
+      [JsonPropertyName("email")]
+      public string? Email { get; set; }
+
+      [JsonPropertyName("title")]
+      public string Title { get; set; } = null!;
+
+      [JsonPropertyName("body")]
+      public string Body { get; set; } = null!;
    }
 }
