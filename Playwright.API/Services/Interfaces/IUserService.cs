840ed4d669c07c0b2052c5f8b67c0f7bef050b57
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-14 10:18:47 +0800

    feat: define methods in IUserService

--- Playwright.API/Services/Interfaces/IUserService.cs
+++ Playwright.API/Services/Interfaces/IUserService.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Playwright.API.Models.User;
 
 namespace Playwright.API.Services.Interfaces
 {
    internal interface IUserService
    {
+      Task<IAPIResponse> GetUsersAsync();
+      Task<IAPIResponse> GetUserAsync(int id);
+      Task<IAPIResponse> CreateUserAsync(UserModel user);
+      Task<IAPIResponse> UpdateUserAsync(UserModel user, int id);
+      Task<IAPIResponse> DeleteUserAsync(int id);
    }
 }
