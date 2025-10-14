c44d03b24250ef2d9e030ac7596a955950d2da72
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-14 10:22:56 +0800

    feat: implement methods in UserService

--- Playwright.API/Services/UserService.cs
+++ Playwright.API/Services/UserService.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Playwright;
+using Playwright.API.Models.User;
+using Playwright.API.Utils;
 
 namespace Playwright.API.Services
 {
    internal class UserService
    {
+      private readonly ApiHelper _apiHelper;
+
+      // Constants
+      private const string endpoint = "/users";
+
+      public UserService(ApiHelper apiHelper)
+      {
+         _apiHelper = apiHelper;
+      }
+
+      public async Task<IAPIResponse> CreateUserAsync(UserModel user) => await _apiHelper.CreateAsync(endpoint, user);
+
+      public async Task<IAPIResponse> DeleteUserAsync(int id) => await _apiHelper.DeleteAsync(endpoint, id);
+
+      public async Task<IAPIResponse> GetUserAsync(int id) => await _apiHelper.GetByIdAsync(endpoint, id);
+
+      public async Task<IAPIResponse> GetUsersAsync() => await _apiHelper.GetAsync(endpoint);
+
+      public async Task<IAPIResponse> UpdateUserAsync(UserModel user, int id) => await _apiHelper.UpdateAsync(endpoint, user, id);
    }
 }
