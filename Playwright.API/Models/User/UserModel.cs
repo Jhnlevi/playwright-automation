3c8b6cdc553a61c4f8256974cbc26d4410c710fb
John Levi Barcenas <jlevi.barcenas@gmail.com>
2025-10-14 10:16:15 +0800

    feat: implement field mappings in UserModel

--- Playwright.API/Models/User/UserModel.cs
+++ Playwright.API/Models/User/UserModel.cs
@@ -1,12 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace Playwright.API.Models.User
 {
    internal class UserModel
    {
+      [JsonPropertyName("id")]
+      public int Id { get; set; }
+
+      [JsonPropertyName("name")]
+      public string Name { get; set; } = null!;
+
+      [JsonPropertyName("username")]
+      public string UserName { get; set; } = null!;
+
+      [JsonPropertyName("email")]
+      public string Email { get; set; } = null!;
+
+      [JsonPropertyName("address")]
+      public Address Address { get; set; } = null!;
+
+      [JsonPropertyName("phone")]
+      public string Phone { get; set; } = null!;
+
+      [JsonPropertyName("website")]
+      public string Website { get; set; } = null!;
+
+      [JsonPropertyName("company")]
+      public Company Company { get; set; } = null!;
+   }
+
+   public class Address
+   {
+      [JsonPropertyName("street")]
+      public string Street { get; set; } = null!;
+
+      [JsonPropertyName("suite")]
+      public string Suite { get; set; } = null!;
+
+      [JsonPropertyName("city")]
+      public string City { get; set; } = null!;
+
+      [JsonPropertyName("zipcode")]
+      public string Zipcode { get; set; } = null!;
+
+      [JsonPropertyName("geo")]
+      public Geo Geo { get; set; } = null!;
+   }
+
+   public class Geo
+   {
+      [JsonPropertyName("lat")]
+      public string Lat { get; set; } = null!;
+
+      [JsonPropertyName("lng")]
+      public string Lng { get; set; } = null!;
+   }
+   public class Company
+   {
+      [JsonPropertyName("name")]
+      public string Name { get; set; } = null!;
+
+      [JsonPropertyName("catchPhrase")]
+      public string CatchPhrase { get; set; } = null!;
+
+      [JsonPropertyName("bs")]
+      public string Bs { get; set; } = null!;
    }
 }
